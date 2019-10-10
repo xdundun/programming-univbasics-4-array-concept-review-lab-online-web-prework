29851c4f0e8e832e679456da96f20152fe506387
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-10 19:27:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   length = array.length
   length.times do|index|
-      if array[index] == value_to_find puts index
+      if array[index] == value_to_find
+        puts index
       end
 end
 
